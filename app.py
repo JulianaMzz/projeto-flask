e7dcfb554831d67d1540bbaac49d7a8043ab31d1
JulianaMzz <juliana24muniz@gmail.com>
2025-04-09 19:49:55 -0300

    chore: app implementada, com blueprint e as intâncias de flask

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-
-
 from flask import Flask
+from alunos.alunos_routes import alunos_routes
+from professores.professores_routes import professores_routes
+from turmas.turmas_routes import turmas_routes
+from reset_routes import reset_routes
 from config import Config
-from alunos.alunos_routes import alunos_blueprint
-from professores.professores_routes import professores_blueprint
-from turmas.turmas_routes import turmas_blueprint
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-app = Flask(__name__)
-app.config.from_object(Config)  
+    app.register_blueprint(alunos_routes)
+    app.register_blueprint(professores_routes)
+    app.register_blueprint(turmas_routes)
+    app.register_blueprint(reset_routes)
 
-app.register_blueprint(alunos_blueprint)
-app.register_blueprint(professores_blueprint)
-app.register_blueprint(turmas_blueprint)
+    return app
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '_main_':
+    app = create_app()
+    app.run(
+        debug=app.config['DEBUG'],
+        host=app.config.get('HOST', '127.0.0.1'),
+        port=app.config.get('PORT', 5000)
+    )
