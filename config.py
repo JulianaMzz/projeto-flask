9f1010a044dd91c615d28c04e99b8163a8e8b58f
JulianaMzz <juliana24muniz@gmail.com>
2025-04-26 13:06:20 -0300

    feat: adicionado arquivo de configuração do flask e banco de dados

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ class Config:
     HOST = '127.0.0.1'
     PORT = 5000
     
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres@db:5432/flask_db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
 class DevelopmentConfig(Config):
     DEBUG = True
     
