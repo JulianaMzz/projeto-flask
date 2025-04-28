cadd9d187f6a1d30977886ffb6fbe53d98370382
JulianaMzz <juliana24muniz@gmail.com>
2025-04-28 12:26:35 -0300

    refactor: integra Swagger e namespaces na criação da aplicação Flask

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
 from flask import Flask
-from alunos.alunos_routes import alunos_routes
-from professores.professores_routes import professores_routes
-from turmas.turmas_routes import turmas_routes
-from reset_routes import reset_routes
-from home_routes import home_routes
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from swagger.swagger_config import api
+from swagger.namespace.alunos_namespace import alunos_ns
+from swagger.namespace.professores_namespace import professores_ns
+from swagger.namespace.turma_namespace import turmas_ns
+
+
+
+
+from reset_routes import reset_routes
+from home_routes import home_routes
+
+
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
     
+ 
+    db.init_app(app)
+
    
+    api.init_app(app)
+
+   
+    api.add_namespace(alunos_ns, path='/alunos')
+    api.add_namespace(professores_ns, path='/professores')
+    api.add_namespace(turmas_ns, path='/turmas')
+
+  
     app.register_blueprint(home_routes)
-    app.register_blueprint(alunos_routes)
-    app.register_blueprint(professores_routes)
-    app.register_blueprint(turmas_routes)
     app.register_blueprint(reset_routes)
 
     return app
