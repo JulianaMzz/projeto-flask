a41854faae6f2eeb12d15569dd7d444c69bf17b5
JulianaMzz <juliana24muniz@gmail.com>
2025-04-28 12:27:30 -0300

    chore: ajusta configuração do SQLAlchemy e variáveis de ambiente

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
+import os
+
 class Config:
     DEBUG = True
     TESTING = False
     SECRET_KEY = 'e93b50c764e469b9c8aab5ad28a93'
     HOST = '127.0.0.1'
     PORT = 5000
-    
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres@db:5432/flask_db")
+
+  
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'instance', 'app.db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-class DevelopmentConfig(Config):
-    DEBUG = True
-    
-class TestingConfig(Config):
-    TESTING = True
-    DEBUG = True
