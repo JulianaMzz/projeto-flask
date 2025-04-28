0f1f71506788861f81ac246c0d3bac8b02f12a6e
JulianaMzz <juliana24muniz@gmail.com>
2025-04-28 12:25:26 -0300

    feat: implementa regras de negócio e persistência de alunos

--- alunos/alunos_model.py
+++ alunos/alunos_model.py
@@ -1,3 +1,4 @@
+
 dados = {
     "alunos": [
         {
@@ -29,16 +30,22 @@ def adicionar_aluno(aluno):
     if any(a["id"] == aluno["id"] for a in dados["alunos"]):
         raise ValueError("Já existe um aluno com esse ID")
     
- 
+   
     if "nota_1_semestre" in aluno and "nota_2_semestre" in aluno:
         aluno["media_final"] = calcular_media(aluno["nota_1_semestre"], aluno["nota_2_semestre"])
-    
+    else:
+        raise ValueError("Notas do semestre não fornecidas")
+
+   
     dados["alunos"].append(aluno)
 
 def atualizar_aluno(id_aluno, novos_dados):
     aluno = aluno_por_id(id_aluno)
-    aluno.update(novos_dados)
     
+   
+    aluno.update(novos_dados)
+
+   
     if "nota_1_semestre" in novos_dados or "nota_2_semestre" in novos_dados:
         nota1 = aluno.get("nota_1_semestre", 0)
         nota2 = aluno.get("nota_2_semestre", 0)
